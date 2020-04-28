ac71c0d61af3af50607a22e6a22f837cac84e220
Daniela <danielita.zerain@gmail.com>
2020-04-28 08:29:56 -0400

    Login Activity

--- app/src/main/java/com/example/yummy/LoginActivity.java
+++ app/src/main/java/com/example/yummy/LoginActivity.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class login_activity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.activity_login);
     }
-
 }
+
