b12c2fb8ab20aea023792d4678367e06ec97b151
Daniela <danielita.zerain@gmail.com>
2020-04-28 16:56:21 -0400

    button

--- app/src/main/java/com/example/yummy/LoginActivity.java
+++ app/src/main/java/com/example/yummy/LoginActivity.java
@@ -2,16 +2,105 @@ package com.example.yummy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     Button loginButton;
+    EditText editTextPassword;
+    EditText editTextUsername;
+    ProgressBar circularBar;
+    TextView forgotPassword;
+    TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton=findViewById(R.id.loginButton);
+        editTextPassword=findViewById(R.id.passwordEditText);
+        editTextUsername=findViewById(R.id.usernameEditText);
+        circularBar=findViewById(R.id.circular);
+        forgotPassword=findViewById(R.id.forgot_password);
+        register=findViewById(R.id.newRegister);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                circularBar.setVisibility(View.VISIBLE);
+                Intent intent= new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registro=new Intent(LoginActivity.this, RegistroActivity.class);
+                startActivity(registro);
+            }
+        });
+
+        editTextUsername.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId== EditorInfo.IME_ACTION_NEXT){
+                    Log.d("TEST", "KEYBOARD DONE");
+                }
+                return false;
+            }
+        });
+
+        editTextPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int action, KeyEvent keyEvent) {
+                if (action == EditorInfo.IME_ACTION_DONE) {
+                    Log.d("TEST", "KEYBOARD DONE");
+                }
+                return false;
+            }
+        });
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        circularBar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
 
