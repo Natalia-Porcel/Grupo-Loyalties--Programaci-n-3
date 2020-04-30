27f23797da0e06002edd7fc24acdc09c1d64f5db
Daniela <danielita.zerain@gmail.com>
2020-04-30 08:57:36 -0400

    Font

--- app/src/main/java/com/example/yummy/RegistroActivity.java
+++ app/src/main/java/com/example/yummy/RegistroActivity.java
@@ -2,13 +2,26 @@ package com.example.yummy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    TextView registro;
+    Typeface chewy;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        registro=findViewById(R.id.textRegistro);
+
+        String font1 = "fuentes/Chewy.ttf";
+        this.chewy=Typeface.createFromAsset(getAssets(),font1);
+
+        registro.setTypeface(chewy);
+
     }
 }
