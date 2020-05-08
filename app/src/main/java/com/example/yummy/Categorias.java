5ee8057e8e5dbb2968e34d956ac26ec19e7602b4
Daniela <danielita.zerain@gmail.com>
2020-05-08 11:40:26 -0400

    pantallaCategorias

--- app/src/main/java/com/example/yummy/Categorias.java
+++ app/src/main/java/com/example/yummy/Categorias.java
@@ -23,17 +23,17 @@ public class Categorias {
     }
 
     public static Categorias[] ITEMS = {
-            new Categorias("Platos Fuertes", R.drawable.comida_rosa),
+            new Categorias("Platos Fuertes", R.drawable.plato_fuerte),
             new Categorias("Acompañamientos" , R.drawable.acompaniamiento),
-            new Categorias("Ensaladas", R.drawable.bebidas),
+            new Categorias("Ensaladas", R.drawable.ensaladas),
             new Categorias("Bebidas", R.drawable.bebidas),
             new Categorias("Salsas y Aderezos", R.drawable.salas),
-            new Categorias("Bocadillos", R.drawable.bebidas),
-            new Categorias("Sopas y Guisados", R.drawable.bebidas),
-            new Categorias("Postres", R.drawable.bebidas),
-            new Categorias("Reposteria", R.drawable.bebidas),
-            new Categorias("Desayuno", R.drawable.bebidas),
-            new Categorias("Comida Boliviana", R.drawable.bebidas),
+            new Categorias("Bocadillos", R.drawable.bocaditos),
+            new Categorias("Sopas y Guisados", R.drawable.sopas),
+            new Categorias("Postres", R.drawable.postres),
+            new Categorias("Reposteria", R.drawable.reposteria),
+            new Categorias("Desayuno", R.drawable.desyuno),
+            new Categorias("Comida Boliviana", R.drawable.plato_tipico),
     };
     public static Categorias getItem(int id) {
         for (Categorias item : ITEMS) {
